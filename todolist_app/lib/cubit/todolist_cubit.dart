786a49983360966d6b8edebca86d51f2515c6c60
hafidzf25 <abdullahnestax@gmail.com>
2025-02-27 10:14:37 +0700

    Push flutter_bloc package

--- todolist_app/lib/cubit/todolist_cubit.dart
+++ todolist_app/lib/cubit/todolist_cubit.dart
@@ -1,8 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part 'todolist_state.dart';
-
-class TodolistCubit extends Cubit<TodolistState> {
+class TodolistCubit extends Cubit {
   TodolistCubit() : super(TodolistInitial());
 }
