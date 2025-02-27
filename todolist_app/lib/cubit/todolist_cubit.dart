7dd66c261882e5a9a4460d9d5b71c800fad8c317
hafidzf25 <abdullahnestax@gmail.com>
2025-02-27 10:30:12 +0700

    Add Error Handling Text Field Empty

--- todolist_app/lib/cubit/todolist_cubit.dart
+++ todolist_app/lib/cubit/todolist_cubit.dart
@@ -5,7 +5,25 @@ class TodoCubit extends Cubit<List<ListTodo>> {
   TodoCubit() : super([]);
 
   void add(String title) {
+    if (title.isEmpty) {
+      addError('Isi Lah Fieldnya');
+      return;
+    }
     final todo = ListTodo(title: title, createdAt: DateTime.now());
     emit([...state, todo]);
+  }
+
+  @override
+  void onChange(Change<List<ListTodo>> change) {
+    // TODO: implement onChange
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+    super.onError(error, stackTrace);
+    print(error);
   }
 }
