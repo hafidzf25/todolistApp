e6194c763bf34305f52633c0b5e3f9cb831e27c7
hafidzf25 <abdullahnestax@gmail.com>
2025-02-27 10:27:43 +0700

    Push Models

--- todolist_app/lib/models/list_todo.dart
+++ todolist_app/lib/models/list_todo.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class ListTodo {
   final String title;
   final DateTime createdAt;
@@ -6,4 +7,7 @@ class ListTodo {
     required this.title,
     required this.createdAt,
   });
+
+  @override
+  String toString() => 'ListTodo(title: $title, createdAt: $createdAt)';
 }
